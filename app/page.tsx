0566c6e5c8940a087513d48ee29677640b869a5a
glori <glori@example.com>
2025-04-09 13:30:28 +0100

    Atualização do site

--- app/page.tsx
+++ app/page.tsx
@@ -4,36 +4,38 @@ import React from 'react';
 
 const HomePage = () => {
   return (
-    <div style={{ fontFamily: 'Poppins, sans-serif', padding: 0, margin: 0, backgroundColor: '#f4f4f4' }}>
+    <div
+      style={{
+        fontFamily: 'Poppins, sans-serif',
+        padding: 0,
+        margin: 0,
+        backgroundColor: '#f4f4f4',
+      }}
+    >
       {/* Cabeçalho */}
       <header
         style={{
           backgroundColor: '#1a3d66',
           color: '#fff',
-          padding: '40px 0',
+          padding: '20px 10px',
           textAlign: 'center',
           boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
         }}
       >
         <img
           src="/images/perfil.jpg"
           alt="Logo da Barbearia Dmitri"
           style={{
-            width: '110px',
+            width: '80px',
             height: 'auto',
             borderRadius: '50%',
             boxShadow: '0 4px 8px rgba(0, 0, 0, 0.2)',
             marginBottom: '10px',
-            transition: 'transform 0.3s ease',
           }}
         />
         <h1
           style={{
-            fontSize: '2.5rem',
-            marginBottom: '10px',
+            fontSize: '1.8rem',
             fontWeight: 'bold',
             textTransform: 'uppercase',
           }}
@@ -43,28 +45,31 @@ const HomePage = () => {
       </header>
 
       {/* Menu de navegação */}
-      <nav style={{ backgroundColor: '#1a3d66', padding: '15px 0', boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)' }}>
+      <nav
+        style={{
+          backgroundColor: '#1a3d66',
+          padding: '10px 0',
+          boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+        }}
+      >
         <ul
           style={{
             display: 'flex',
-            justifyContent: 'center',
+            justifyContent: 'space-around',
             listStyle: 'none',
             padding: 0,
             margin: 0,
           }}
         >
           {['Serviços', 'Agendamento', 'Contato'].map((item) => (
-            <li key={item} style={{ margin: '0 25px' }}>
+            <li key={item}>
               <a
                 href={item === 'Serviços' ? '/servicos' : `#${item.toLowerCase()}`}
                 style={{
                   color: '#fff',
                   textDecoration: 'none',
-                  fontSize: '1.2rem',
-                  transition: 'color 0.3s, transform 0.3s',
+                  fontSize: '1rem',
                 }}
-                onMouseOver={(e) => (e.target as HTMLElement).style.color = '#5cb85c'}
-                onMouseOut={(e) => (e.target as HTMLElement).style.color = '#fff'}
               >
                 {item}
               </a>
@@ -74,28 +79,27 @@ const HomePage = () => {
       </nav>
 
       {/* Galeria de cortes */}
-      <section id="galeria" style={{ marginTop: '40px', padding: '0 20px' }}>
-        <h2 style={{ color: '#1a3d66', textAlign: 'center', fontSize: '2rem', fontWeight: 'bold' }}>
+      <section id="galeria" style={{ marginTop: '20px', padding: '10px' }}>
+        <h2
+          style={{
+            color: '#1a3d66',
+            textAlign: 'center',
+            fontSize: '1.5rem',
+            fontWeight: 'bold',
+          }}
+        >
           Galeria de Cortes
         </h2>
         <div
           style={{
             display: 'grid',
-            gridTemplateColumns: 'repeat(auto-fill, minmax(250px, 1fr))',
-            gap: '30px',
-            marginTop: '30px',
+            gridTemplateColumns: 'repeat(auto-fill, minmax(150px, 1fr))',
+            gap: '15px',
+            marginTop: '20px',
           }}
         >
           {['corte4.jpg', 'corte2.jpg', 'corte3.jpg', 'corte5.jpg'].map((img, i) => (
-            <div
-              key={i}
-              style={{
-                borderRadius: '15px',
-                overflow: 'hidden',
-                boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-                transition: 'transform 0.3s ease, box-shadow 0.3s ease',
-              }}
-            >
+            <div key={i} style={{ borderRadius: '10px', overflow: 'hidden' }}>
               <img
                 src={`/images/${img}`}
                 alt={`Corte ${i + 1}`}
@@ -103,7 +107,7 @@ const HomePage = () => {
                   width: '100%',
                   height: 'auto',
                   objectFit: 'cover',
-                  borderRadius: '15px',
+                  borderRadius: '10px',
                 }}
               />
             </div>
@@ -112,10 +116,10 @@ const HomePage = () => {
       </section>
 
       {/* Seção de agendamento */}
-      <section id="agendamento" style={{ marginTop: '40px', textAlign: 'center' }}>
-        <h2 style={{ color: '#1a3d66', fontSize: '2rem', fontWeight: 'bold' }}>Agendamento Online</h2>
-        <p style={{ fontSize: '1.2rem', fontWeight: 300, margin: '20px 0' }}>
-          Marque seu horário para não perder tempo. Clique abaixo para marcar:
+      <section id="agendamento" style={{ marginTop: '20px', textAlign: 'center' }}>
+        <h2 style={{ color: '#1a3d66', fontSize: '1.5rem', fontWeight: 'bold' }}>Agendamento Online</h2>
+        <p style={{ fontSize: '.9rem', marginBottom: '.9rem' }}>
+          Marque seu horário para não perder tempo:
         </p>
         <a
           href="https://calendly.com/gloriajoaopaulo8/30min"
@@ -125,20 +129,10 @@ const HomePage = () => {
             textDecoration: 'none',
             color: '#fff',
             backgroundColor: '#0055a5',
-            padding: '15px 30px',
-            borderRadius: '30px',
-            fontSize: '1.4rem',
+            padding: '.7rem 1.5rem',
+            borderRadius: '.7rem',
+            fontSize: '.9rem',
             display: 'inline-block',
-            transition: 'background-color 0.3s ease, transform 0.3s ease',
-            boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)',
-          }}
-          onMouseOver={(e) => {
-            (e.target as HTMLElement).style.backgroundColor = '#004285';
-            (e.target as HTMLElement).style.transform = 'scale(1.05)';
-          }}
-          onMouseOut={(e) => {
-            (e.target as HTMLElement).style.backgroundColor = '#0055a5';
-            (e.target as HTMLElement).style.transform = 'scale(1)';
           }}
         >
           Agende agora
@@ -146,9 +140,9 @@ const HomePage = () => {
       </section>
 
       {/* Seção de contato */}
-      <section id="contato" style={{ marginTop: '40px', textAlign: 'center', marginBottom: '60px' }}>
-        <h2 style={{ color: '#1a3d66', fontSize: '2rem', fontWeight: 'bold' }}>Contato</h2>
-        <p style={{ fontSize: '1.2rem' }}>
+      <section id="contato" style={{ marginTop: '.7rem', textAlign: 'center' }}>
+        <h2 style={{ color: '#1a3d66', fontSize: '1.5rem', fontWeight: 'bold' }}>Contato</h2>
+        <p style={{ fontSize: '.9rem' }}>
           Ligue para: <a href="tel:+351968566669" style={{ color: '#0055a5' }}>+351 968 566 669</a>
         </p>
       </section>
@@ -159,11 +153,11 @@ const HomePage = () => {
           backgroundColor: '#1a3d66',
           color: '#fff',
           textAlign: 'center',
-          padding: '30px 20px',
-          fontSize: '0.9rem',
+          padding: '20px 10px',
+          fontSize: '0.7rem',
         }}
       >
-        <p>Endereço: Rua Tito de Morais Loja 3 A, Lisboa, Portugal, 1750-455</p>
+        
         <p style={{ marginTop: '10px' }}>&copy; {new Date().getFullYear()} Barbearia Dmitri. Todos os direitos reservados.</p>
       </footer>
     </div>
